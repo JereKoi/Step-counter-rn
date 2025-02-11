e0e4b66070e4f04a40c39a146142b0e81146fd79
Jere <jerekoivisto7@gmail.com>
2025-02-11 17:25:15 +0200

    Working on styling of the step tracker.

--- app/(tabs)/app.tsx
+++ app/(tabs)/app.tsx
@@ -1,60 +1,62 @@
-import { Image, StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 import { Pedometer } from "expo-sensors";
 import React, { useEffect, useState } from "react";
+import CircularProgress from "react-native-circular-progress-indicator";
 
 export default function App() {
   const [steps, setSteps] = useState(0);
   const [PedometerAvailability, setPedometerAvailability] = useState("");
 
-Pedometer.isAvailableAsync().then(
-  (result) => {
-    setPedometerAvailability(String(result))
-  },
-  (error) => {
-    setPedometerAvailability(error);
-  }
-)
+  Pedometer.isAvailableAsync().then(
+    (result) => {
+      setPedometerAvailability(String(result));
+    },
+    (error) => {
+      setPedometerAvailability(error);
+    }
+  );
 
-
-useEffect(() => {
-  subscribe();
-}, [])
+  useEffect(() => {
+    subscribe();
+  }, []);
 
   const subscribe = () => {
     const subscribtion = Pedometer.watchStepCount((result) => {
-      setSteps(result.steps)
-    })
-  }
+      setSteps(result.steps);
+    });
+  };
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.container}
-        />
-      }>
-      <ThemedView style={styles.container}>
-      <ThemedText type="default">Is Pedometer available on the device:{PedometerAvailability} </ThemedText>
-        <ThemedText type="default">{steps}</ThemedText>
-
-        <HelloWave />
-      </ThemedView>
-    </ParallaxScrollView>
+    <view style={styles.container}>
+      <view style={{ flex: 1, justifyContent: "center " }}>
+        <h1>Is Pedometer available on the device:{PedometerAvailability} </h1>
+        <view>
+          <CircularProgress
+            value={steps}
+            maxValue={10000}
+            radius={210}
+            activeStrokeColor={"#2465FD"}
+            activeStrokeSecondaryColor={"#C25AFF"}
+            inActiveStrokeOpacity={0.5}
+            inActiveStrokeWidth={40}
+            activeStrokeWidth={40}
+            title={"Step count"}
+            titleColor={"#ECF0F1"}
+            titleStyle={{ fontWeight: "bold" }}
+          />
+        </view>
+      </view>
+    </view>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: 15,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    color: "white",
+    backgroundColor: "rgba(255, 255, 255, 255, 0.5)",
+    alignSelf: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
